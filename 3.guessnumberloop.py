c0cb90a5b9e64c1d710ce0666d0df542ee7498eb
Thanakorn boonson <thanakornboonsorn@gmail.com>
2026-04-13 10:11:01 +0000

    add number guessing game

--- 3.guessnumberloop.py
+++ 3.guessnumberloop.py
@@ -1,12 +1,13 @@
 import random
-lucky_number= random.randint(1,11)
-while True:
-    guess = int(input("enternumber"))
 
-    lucky_number = random.randint(1, 10)
-       print("ถั่วต้มแร้ว , สวดยอด")
-       break
-elif: guess > lucky_number:
-    print("โอโห , ไก่งะ")
-else:
-    print("ไปไปพอเถอะ")
+lucky_number= random.randint(1,10)
+
+while True:
+    guess = int(input("enter number:"))
+    if guess == lucky_number:
+        print("ถั่วต้มแร้ว, สวดยอด")
+        break  # ทายถูกแล้วออกจากลูป
+    elif guess > lucky_number:
+        print("โอโห, เยอะไปไก่งะ")
+    else:
+        print("น้อยไป ไปๆ พอเถอะ")
