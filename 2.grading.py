e6716f93e61864fa3cc5be21f5912eb519bd39ca
Thanakorn boonson <thanakornboonsorn@gmail.com>
2026-04-13 08:55:30 +0000

    add grading program

---
+++ 2.grading.py
@@ -0,0 +1,14 @@
+# 2.grading.py
+# โปรแกรมคำนวณเกรดจากคะแนน
+score = float(input("entet_your_score"))
+
+if score >= 80:
+    print("A")
+elif score >= 70:
+    print("B")
+elif score >= 60:
+    print("c")
+elif score >= 50:
+    print("D")
+else:
+    print("F")
--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-#Condinations/loop//Coding-learning-All-days 
-score final
-if = float(input("yourscore")) >= 80:
-    print("A")
-eilf <= 70:
-print("B")
-else <= 60:
- print('C')
